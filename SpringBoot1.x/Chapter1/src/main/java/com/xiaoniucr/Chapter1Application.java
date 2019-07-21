b35f377d80897f008b55e9c545e8f373c16055f5
yanghl <2357574009@qq.com>
2019-07-21 16:02:16 +0800

    SpringBoot第一章

--- SpringBoot1.x/Chapter1/src/main/java/com/xiaoniucr/Chapter1Application.java
+++ SpringBoot1.x/Chapter1/src/main/java/com/xiaoniucr/Chapter1Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Chapter1Application {
 
+    //使用内置tomcat容器启动项目
     public static void main(String[] args) {
         SpringApplication.run(Chapter1Application.class, args);
     }
